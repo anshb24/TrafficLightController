254ae5897351cb4667854d42d9f3228f26087c28
Ansh Bhatnagar <118305974+anshb24@users.noreply.github.com>
2023-05-08 11:16:08 -0400

    Add files via upload

---
+++ header1.h
@@ -0,0 +1,22 @@
+#define DELAY 2000
+#define ON 0x01
+#define OFF 0x00
+
+#define NS1 (1U<<10) //pin 16 offset 
+#define NS2 (1U<<11) //pin 17 offset
+#define ES1 (1U<<12) //pin 18 offset
+#define ES2 (1U<<13) //pin 19 offset
+
+#define NSR (1U<<1)       //pin 9 offset        
+#define NSY (1U<<2)       //pin 10 offset
+#define NSG (1U<<18)       //pin 2 offset
+#define EWR (1U<<19)       //pin 3 offset    
+#define EWY (1U<<20)       //pin 4 offset
+#define EWG (1U<<21)       //pin 5 offset 
+#define PR  (1U<<22)       //pin 6 offset
+#define PG  (1U<<23)       //pin 7 offset
+
+void setupGPIO();
+int setLED(int color, int state);
+void delay(int milliseconds);
+int checkBot(); 
---
+++ main.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include "header1.h" 
+int main()
+{
+int state = 1; 
+int error = 0;
+int count = 0;
+setupGPIO();
+while(1)
+{
+    switch(state)
+        {
+
+            case 1: //initial state - both ways red light
+                setLED(NSR|EWR|PR, ON);
+                delay(3000);
+                if(checkBot(NS1))
+                {
+                    setLED(PR,OFF);
+                    setLED(PG,ON);
+                    delay(15000);
+                    setLED(PG,OFF);
+                    state = 1;
+                }
+                else if(!checkBot(NS1))
+                {                
+                    delay(10000); //after 10 seconds
+                    state = 2;
+
+                }
+                break;
+            
+            case 2: // North South Green , East West Red
+                   setLED(NSR, OFF);                   
+                   setLED(NSG|PR, ON);
+                   delay(3000); //
+                   if(checkBot(NS1))
+                    {
+                        setLED(NSG,OFF);
+                        while(!error)
+                        {
+                            setLED(NSY,ON);
+                            delay(500);
+                            error = setLED(NSY,OFF);
+                            delay(500);
+                            count++;
+                            if(count>10)
+                            {
+                            break;
+                            }
+                        }
+                        setLED(PR,OFF);
+                        setLED(NSR|PG,ON);
+                        delay(15000);
+                        setLED(PG,OFF);
+                        state = 1;
+                    }
+                else if(!checkBot(NS1))
+                { 
+                   delay(20000); //after 20 seconds
+                   state = 3;
+                }
+                   break;
+            
+             case 3: // North South Yellow after 3 seconds, North South red, East west red
+                   setLED(NSG|NSR, OFF);
+                   setLED(NSY|PR, ON);
+                   delay(3000); // 3 seconds
+                   if(checkBot(NS1))
+                    {
+                    setLED(PR|NSY,OFF);
+                    setLED(PG|NSR,ON);
+                    delay(15000);
+                    setLED(PG,OFF);
+                    state = 2;
+                    }
+                    else if(!checkBot(NS1))
+                    {
+                   state = 4;
+                    }
+                   break;
+            case 4:
+                   setLED(NSY, OFF);
+                   setLED(NSR|PR, ON);
+                   delay(5000); // 5 seconds
+                   if(checkBot(NS1))
+                    {
+                    setLED(PR,OFF);
+                    setLED(PG,ON);
+                    delay(15000);
+                    setLED(PG,OFF);
+                    state = 3;
+                    }
+                    else if(!checkBot(NS1))
+                    {
+                    state = 5;
+                    }
+                    break;
+            
+             case 5: // North South red, east west green
+                   setLED(EWR, OFF);
+                   setLED(EWG|PR, ON);
+                   delay(3000); 
+                    if(checkBot(NS1))
+                    {
+                        setLED(EWG,OFF);
+                        count=0;
+                        while(!error)
+                        {
+                            setLED(EWY,ON);
+                            delay(500);
+                            error = setLED(EWY,OFF);
+                            delay(500);
+                            count++;
+                            if(count>10)
+                            {
+                            break;
+                            }
+                        }
+                        setLED(PR,OFF);
+                        setLED(EWR|PG,ON);
+                        delay(15000);
+                        setLED(PG,OFF);
+                        state = 4;
+                    }
+                    else if(!checkBot(NS1))
+                    {
+                   delay(20000);
+                   state = 6;
+                    }
+                   break;
+            
+             case 6: // after 20 seconds
+                   setLED(EWG, OFF);
+                   setLED(EWY, ON);
+                   delay(3000);
+                   if(checkBot(NS1))
+                    {
+                    setLED(PR|EWY,OFF);
+                    setLED(PG|EWR,ON);
+                    delay(15000);
+                    setLED(PG,OFF);
+                    state = 5;
+                    }
+                    else if(!checkBot(NS1))
+                    {
+                   setLED(EWY, OFF);
+                   setLED(EWR, ON);
+                   delay(5000);     
+                   state = 1;
+                    }
+                   break;
+            
+        }
+    }
+  }
